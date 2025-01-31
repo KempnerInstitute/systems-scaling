a01df65eeeb12702ea6097977a3f373ff5683a1e
Hither1 <suhither@gmail.com>
2025-01-31 17:15:34 -0500

    icml submitted

--- nanoGPT/model_mx.py
+++ nanoGPT/model_mx.py
@@ -85,13 +85,13 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc    = Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.gelu    = nn.GELU()
+        # self.gelu    = nn.GELU()
         self.c_proj  = Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = self.gelu(x)
+        x = gelu(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
